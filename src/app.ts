9614c70bd756ef148e591d756e1186402060f03f
HyeonTae Jang <gusxo999@gmail.com>
2025-06-16 14:00:26 +0900

    프로젝트 시작전 추가 3

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import YAML from "yamljs";
 import SwaggerUi from "swagger-ui-express";
 import express from "express";
+import {
+  defaultNotFoundHandler,
+  globalErrorHandler,
+} from "./controllers/errorController";
 
 const app = express();
 app.use(express.json());
@@ -9,6 +13,8 @@ app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
+app.use(defaultNotFoundHandler);
+app.use(globalErrorHandler);
 // swagger
 if (process.env.NODE_ENV !== "production") {
   const swaggerDocument = YAML.load("./swagger/swagger.yaml");
--- src/structs/commonStructs.ts
+++ src/structs/commonStructs.ts
@@ -5,27 +5,21 @@ import {
   string,
   defaulted,
   optional,
-  enums,
   nonempty,
-  define,
   Infer,
   pattern,
-} from 'superstruct';
+} from "superstruct";
 export const emailRegExp = pattern(
   string(),
   // /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
-  /^[\w.-]+@([\w.-]+\.)+[\w]{2,4}$/i,
+  /^[\w.-]+@([\w.-]+\.)+[\w]{2,4}$/i
 );
 
 export const phoneNumberRegExp = pattern(string(), /^\d{2,3}-\d{3,4}-\d{4}$/);
 
-export const integerString = coerce(integer(), string(), (value) => parseInt(value));
-
-export const CarIdParamsStruct = object({
-  carId: integerString,
-});
-
-const urlRegExp = /^(https?:\/\/)/;
+export const integerString = coerce(integer(), string(), (value) =>
+  parseInt(value)
+);
 
 export const PageParamsStruct = object({
   page: defaulted(integerString, 1),
