84075151704e4fa76fb535055c0f46908435c66d
hapyvirus <serenity2011@naver.com>
2025-06-18 18:11:14 +0900

    feat: 내 정보 조회 기능 생성

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -8,3 +8,11 @@ export const createUser: RequestHandler = async (req, res) => {
   const user = await userService.createUser(data);
   res.status(201).send(user);
 };
+
+export const getUser: RequestHandler = async (req, res) => {
+  const userId = req.user.id;
+
+  const mypage = await userService.getMydata(userId);
+
+  res.status(200).send(mypage);
+};
