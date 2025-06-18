b3c8c8e89dffbb121e6ff7ff3ea340132009c13b
JungEun Park <jungeunp97@naver.com>
2025-06-18 09:45:27 +0900

    [feat] store 생성 API 구현

---
+++ src/structs/storeStructs.ts
@@ -0,0 +1,23 @@
+import {
+  nonempty,
+  nullable,
+  object,
+  optional,
+  refine,
+  size,
+  string,
+} from "superstruct";
+
+const phoneNumber = refine(
+  size(nonempty(string()), 9, 20),
+  "phoneNumber",
+  (value) => /^0\d{1,2}-\d{3,4}-\d{4}$/.test(value)
+);
+
+export const createStoreBodyStruct = object({
+  name: size(nonempty(string()), 2, 50),
+  address: size(nonempty(string()), 2, 100),
+  phoneNumber: phoneNumber,
+  content: size(nonempty(string()), 2, 300),
+  image: optional(size(string(), 0, 2048)),
+});
