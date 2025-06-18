e7e2b2768bb7f8b89d5936c7f55ca6efab8e6cf2
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-06-18 13:12:35 +0900

    'fix:inquiryDummy'

--- test/inquiryDummy.ts
+++ test/inquiryDummy.ts
@@ -1,5 +1,6 @@
 // User
 
+import { Decimal } from "@prisma/client/runtime/library";
 import { UserType } from "@prisma/client";
 
 export const buyerUser = {
@@ -9,7 +10,7 @@ export const buyerUser = {
   password: "password1234",
   type: UserType.BUYER,
   point: 0,
-  totalAmount: 0,
+  totalAmount: new Decimal(0),
   createdAt: new Date(),
   updatedAt: new Date(),
 };
@@ -21,7 +22,7 @@ export const sellerUser = {
   password: "password1234",
   type: UserType.SELLER,
   point: 0,
-  totalAmount: 0,
+  totalAmount: new Decimal(0),
   createdAt: new Date(),
   updatedAt: new Date(),
 };
@@ -32,7 +33,7 @@ export const sellerUser2 = {
   password: "password1234",
   type: UserType.SELLER,
   point: 0,
-  totalAmount: 0,
+  totalAmount: new Decimal(0),
   createdAt: new Date(),
   updatedAt: new Date(),
 };
