8b03978449c0a0760e7f9a2b557ece609cda10aa
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-06-25 13:38:15 +0900

    'feat:cart,cartItem_get,delete'

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -7,10 +7,14 @@ import {
   getCart,
   patchData,
   CartItemSizes,
+  deleteData,
+  getIdCartItem,
+  cartDataFind,
 } from "../repositories/cartRepository";
 import { CartData, CartList, CartItemData } from "../types/cartType";
 import { cartDTO, cartListDTO, cartItemDTO } from "../lib/dto/cartDto";
 import { cartBodyType } from "../structs/cartStructs";
+import ForbiddenError from "../lib/errors/ForbiddenError";
 
 export async function postCart(user: string): Promise<cartDTO> {
   const userData = await userRepository.findById(user);
@@ -83,6 +87,69 @@ export async function patchCart(
     ...updatedItem,
     cart: {
       ...updatedItem.cart,
+      quantity: quantity,
+    },
+  };
+
+  return new cartItemDTO(result);
+}
+
+export async function deleteCartItem(user: string, params: string) {
+  const userData = await userRepository.findById(user);
+  if (!userData) {
+    console.log("userData 호출");
+    throw new NotFoundError("user", user);
+  }
+
+  const cartData = await cartList(user);
+
+  if (!cartData) {
+    console.log("cartData 호출");
+    throw new NotFoundError("cart", user);
+  }
+
+  const cartItemData = await getIdCartItem(params);
+
+  if (!cartItemData) {
+    console.log("cartItemData 호출");
+    throw new NotFoundError("cartItem", params);
+  }
+
+  if (cartData.id !== cartItemData.cartId) {
+    throw new ForbiddenError();
+  }
+
+  await deleteData(params);
+}
+
+export async function cartItemDetail(user: string, params: string) {
+  const userData = await userRepository.findById(user);
+  if (!userData) {
+    throw new NotFoundError("user", user);
+  }
+
+  const cartData = await cartList(user);
+
+  if (!cartData) {
+    throw new NotFoundError("cart", user);
+  }
+
+  const cartDetail = await cartDataFind(params);
+
+  if (!cartDetail) {
+    throw new NotFoundError("cartItem", user);
+  }
+
+  if (cartData.id !== cartDetail.cartId) {
+    throw new ForbiddenError();
+  }
+
+  const quantity = await cartItemCount(cartData.id);
+
+  const result: CartItemData = {
+    ...cartDetail,
+    cart: {
+      ...cartDetail.cart,
       quantity: quantity,
     },
   };
