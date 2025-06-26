12eb42332bfe5d4e79a6aca9f6f447217636eed0
JungEun Park <jungeunp97@naver.com>
2025-06-26 13:27:18 +0900

    [feat] 리뷰 목록 조회

--- src/routers/reviewRouter.ts
+++ src/routers/reviewRouter.ts
@@ -5,11 +5,13 @@ import {
   createReview,
   deleteReview,
   getReviewInfo,
+  getReviewList,
   updateReview,
 } from "../controllers/reviewController";
 
 export const reviewRouter = express.Router();
 reviewRouter.post("/:id/reviews", authMiddleware, asyncHandler(createReview));
+reviewRouter.get("/:id/reviews", asyncHandler(getReviewList));
 reviewRouter.patch("/:id", authMiddleware, asyncHandler(updateReview));
 reviewRouter.get("/:id", asyncHandler(getReviewInfo));
 reviewRouter.delete("/:id", authMiddleware, asyncHandler(deleteReview));
